4513e4b2c4e365e8f4866e68bf12c6cfbc4debba
oumaimat <turki.oumaima@gmail.com>
2015-08-09 19:08:29 +0200

    Version 0 Git trial  : GenericDAO implementation Insert, GetAll methods tested

---
+++ DAO/GenericDAO.py
@@ -0,0 +1,86 @@
+__author__ = 'OTurki'
+
+import pymongo
+
+
+class ConnectionToDatabase :
+
+    database = None
+
+    #penser à créer un pool de connections réutilisables vers la base
+    #solution actuelle non envisageable en prod
+
+    def __init__(self):
+        self.connectToDatabase()
+
+
+    def connectToDatabase(self) :
+        # Connexion au serveur de Mongo DB
+        db_conn=None
+
+        try:
+            db_conn=pymongo.MongoClient()
+            print("Connected successfully!!!")
+            print(db_conn)
+        except pymongo.errors.ConnectionFailure :
+            print("Could not connect to MongoDB: %s")
+
+
+        # Connexion a la base du projet
+        db = db_conn["project_database"]
+
+        #function result
+        ConnectionToDatabase.database = db
+
+
+    def getCollection(self, collection_name):
+
+        #Connexion a la collection
+        collection = ConnectionToDatabase.database[collection_name]
+
+        #function result
+        return collection
+
+class GenericDAO :
+
+    connectionToDatabase = ConnectionToDatabase()
+
+    # Insérer un enregistrement dans une collection
+    def insertObjectInCollection(self, collectionName, object):
+
+        collection =  GenericDAO.connectionToDatabase.getCollection(collectionName)
+        insertionResult = collection.insert_one(object)
+
+        return insertionResult
+
+    # Supprimer un enregistrement dans une collection
+    def removeOneObjectFromCollection(self,collectionName, objectID ):
+
+        collection =  GenericDAO.connectionToDatabase.getCollection(collectionName)
+        removeResult = collection.remove(objectID)
+
+        return removeResult
+
+    # Supprimer un ou plusieurs enregistrements dans une collection
+    def removeOneObjectFromCollection(self,collectionName, objectCriterias):
+
+        collection =  GenericDAO.connectionToDatabase.getCollection(collectionName)
+        removeResult = collection.remove(objectCriterias)
+
+        return removeResult
+
+    # Méthode à n'utiliser que dans certains cas par la suite à cause du très grand nombre d'enregistrements
+    def getAllRecords(self, collectionName):
+
+        collection = GenericDAO.connectionToDatabase.getCollection(collectionName)
+        recordsList = list(collection.find())
+
+        return recordsList
+
+    # Extraire un enregistrement
+    def getOneRecord(self, collectionName, object):
+
+        collection = GenericDAO.connectionToDatabase.getCollection(collectionName)
+        foundObject = collection.find_one(object)
+
+        return foundObject
--- mongod_test_0.py
+++ mongod_test_0.py
@@ -1,73 +1,78 @@
 __author__ = 'OTurki'
 
-import pymongo
 from Model.User import  User
+from DAO.GenericDAO import GenericDAO
 
-def connectToDatabaseServer() :
-    # Connexion au serveur de Mongo DB
-    db_conn=None
-
-    try:
-        db_conn=pymongo.MongoClient()
-        print("Connected successfully!!!")
-        print(db_conn)
-    except pymongo.errors.ConnectionFailure :
-        print("Could not connect to MongoDB: %s" % e)
-
-    #function result
-    return db_conn
-
-def connectToDatabase(db_conn,database_name) :
-
-    # Connexion a la base du projet
-
-    db = db_conn[database_name]
-
-    #function result
-    return db
-
-def getCollection(db, collection_name) :
-
-    #Connexion a la collection
-
-    collection = db[collection_name]
-
-    #function result
-    return collection
-
-def getAllUsers(users_collection) :
-
-    for user in users_collection.find():
-        print(user)
+#
+# def connectToDatabaseServer() :
+#     # Connexion au serveur de Mongo DB
+#     db_conn=None
+#
+#     try:
+#         db_conn=pymongo.MongoClient()
+#         print("Connected successfully!!!")
+#         print(db_conn)
+#     except pymongo.errors.ConnectionFailure :
+#         print("Could not connect to MongoDB: %s" % e)
+#
+#     #function result
+#     return db_conn
+#
+# def connectToDatabase(db_conn,database_name) :
+#
+#     # Connexion a la base du projet
+#
+#     db = db_conn[database_name]
+#
+#     #function result
+#     return db
+#
+# def getCollection(db, collection_name) :
+#
+#     #Connexion a la collection
+#
+#     collection = db[collection_name]
+#
+#     #function result
+#     return collection
+#
+# def getAllUsers(users_collection) :
+#
+#     for user in users_collection.find():
+#         print(user)
 
 #for user in users_collection.find() :
 #print(user["userPseudo"])
 
-db_conn = connectToDatabaseServer()
-db = connectToDatabase(db_conn, "project_database")
-users_collection = getCollection(db, "users")
 
+genericDAO = GenericDAO()
 
+collectionName = "users"
 
-#inserer un user test dans la collection
-userToInsert = User("pseudo test 3","nom test 3", "prénom test 3")
+# tester la méthode insertObjectInCollection
+#
+# inserer un user test dans la collection
+# userToInsert = User("pseudo test 4","nom test 4", "prénom test 4")
+#
+# print (genericDAO.insertObjectInCollection(collectionName, userToInsert.parseToDict()))
 
+# tester la méthode getAllRecords
+#
+for user in GenericDAO.getAllRecords(collectionName):
+    print(user)
 
-pref1 = {}
-pref1["produit"]="yaourt glacé"
-pref1["prix"] =  11.5
+#Tester les méthodes suppression et getOne
 
-pref2 = {}
-pref2["produit"]="donut"
-pref2["prix"] =  3.25
-
-prefs = []
-prefs.append(pref1)
-prefs.append(pref2)
-
-
-#insérer un enregistrement
-#insertOK = users_collection.insert_one(userToInsert.parseToDict())
-
-getAllUsers(users_collection)
+# Données test, à examiner plus tard
+# pref1 = {}
+# pref1["produit"]="yaourt glacé"
+# pref1["prix"] =  11.5
+#
+# pref2 = {}
+# pref2["produit"]="donut"
+# pref2["prix"] =  3.25
+#
+# prefs = []
+# prefs.append(pref1)
+# prefs.append(pref2)
 
