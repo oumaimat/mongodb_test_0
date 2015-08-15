558a73061dcf36f9df6152477c76258b3c844ce0
oumaimat <turki.oumaima@gmail.com>
2015-08-15 14:23:26 +0200

    Web services implementation
    Get_users web service : DONE

--- web_service_02.py
+++ web_service_02.py
@@ -1,3 +1,5 @@
+from pandas.tseries.offsets import DateOffset
+
 __author__ = 'OTurki'
 
 from Model.User import  User
@@ -9,36 +11,6 @@ import json
 import datetime
 
 
-app = Flask(__name__)
-
-genericDAO = GenericDAO()
-
-collectionName = "users"
-
-usersList1111 = [
-{ "userPseudo" : "user1", "userNom" : "nom1", "userPrenom" : "prenom1" },
-{ "userPseudo" : "user2", "userNom" : "nom2", "userPrenom" : "prenom2" },
-{  "userPseudo" : "pseudo test", "userNom" : "nom test", "userPrenom" : "prenom test" },
-{ "userPseudo" : "pseudo test 1","userPrenom" : "prenom test 1", "userNom" : "nom test 1" }
-]
-
-@app.route('/')
-def api_root():
-    return 'Welcome'
-
-@app.route('/servus/users', methods=['GET'])
-def get_tasks():
-    usersList = GenericDAO.getAllObjects(collectionName)
-
-    res = json.dump(usersList1111, default=MongoJsonEncoder)
-
-    return res
-    #return jsonify({'users': usersList1111})
-
-if __name__ == '__main__':
-    app.run()
-
-
 class MongoJsonEncoder(json.JSONEncoder):
     def default(self, objectToEncode):
         if isinstance(objectToEncode, (datetime.datetime, datetime.date)):
@@ -46,3 +18,31 @@ class MongoJsonEncoder(json.JSONEncoder):
         elif isinstance(objectToEncode, ObjectId):
             return str(objectToEncode)
         return json.JSONEncoder.default(self, objectToEncode)
+
+app = Flask(__name__)
+
+genericDAO = GenericDAO()
+
+collectionName = "users"
+
+@app.route('/')
+def api_root():
+    return 'Welcome'
+
+@app.route('/servus/users', methods=['GET'])
+def get_users():
+    usersList = GenericDAO.getAllObjects(collectionName)
+
+    res = json.dumps(usersList, cls=MongoJsonEncoder)
+
+    return res
+
+
+test = get_users()
+
+print(test)
+
+# if __name__ == '__main__':
+#     app.run()
+
+
