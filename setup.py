eaa42c27fd5960b43dc274054d6a2e36f49e9178
oumaimat <turki.oumaima@gmail.com>
2015-08-19 17:47:39 +0200

    Commit de l'espoir
    Ya rab

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='mongodb_test_0',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='OTurki',
+    author_email='',
+    description=''
+)
